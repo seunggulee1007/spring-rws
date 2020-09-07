9b13cab32ef4d19de8e6aea39b57e5fa0a9dd7ff
leesg <leesg107@naver.com>
2020-09-07 14:35:16 +0900

    데이터 베이스 변경
    데이터베이스 및 vue 파일 변경

--- src/main/java/com/inno/rws/config/WebSecurityConfig.java
+++ src/main/java/com/inno/rws/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()                                        // 다음 리퀘스트에 대한 사용권한 체크
                     .antMatchers("/*", "/api/signUp", "/api/signIn")
                         .permitAll()                                        // 가입 및 인증 주소는 누구나 접근가능
-                    .antMatchers("/api/*")
+                    .antMatchers("/api/**")
                         .hasRole("USER")                                    // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
             .and()
                 .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
