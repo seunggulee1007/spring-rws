cd722d5930085168300047b8cb451eb4f4e17a90
leeseunggu <leesg107@naver.com>
2020-10-06 19:30:40 +0900

    로그 중 필요없는 내역 삭제
    복붙 중 잘 못 복사된 smilegate로그 삭제

--- src/main/java/com/inno/rws/model/vo/CommonVO.java
+++ src/main/java/com/inno/rws/model/vo/CommonVO.java
@@ -2,16 +2,18 @@ package com.inno.rws.model.vo;
 
 import lombok.Data;
 
+import java.sql.Date;
+
 @Data
 public class CommonVO {
 
     /** 등록자 */
     private String crtId;
     /** 등록일자 */
-    private String crtDtm;
+    private Date crtDtm;
     /** 수정자 */
     private String chgId;
     /** 수정일자 */
-    private String chgDtm;
+    private Date chgDtm;
     
 }
